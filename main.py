6e8891f25f218af7c769d0cf24ccb34844a1140c
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-07-01 15:53:11 +0500

    Fix loading environment variables in main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from aiogram.filters import Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
-yldan o'zgaruvchilarni yuklashvvv
 load_dotenv()vf
 dv
 # Bot va Dispatcher obyektlarini yaratish
